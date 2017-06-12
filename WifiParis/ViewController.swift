304210c430f66e07baa76ad633cb6fb4b349c3c4
Julien Bremeersch <julienbremeersch@macbook-pro-de-julien.local>
2017-06-12 02:18:49 -0700

    Initial Commit

---
+++ WifiParis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WifiParis
+//
+//  Created by Julien Bremeersch on 12/06/2017.
+//  Copyright © 2017 Julien Bremeersch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
